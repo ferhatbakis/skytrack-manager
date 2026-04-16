205c0f2ef3a4c9c62ef327e70491003e1a7135d2
Ferhat Bakış <bakisferhat@gmail.com>
2026-04-16 21:21:13 +0300

    add WPF UI for flight logging and data entry

---
+++ AddFlightWindow.cs
@@ -0,0 +1,18 @@
+﻿private void btnSave_Click(object sender, RoutedEventArgs e)
+{
+    var newLog = new FlightLog
+    {
+        OriginICAO = txtOrigin.Text,
+        DestinationICAO = txtDestination.Text,
+        DurationMinutes = int.Parse(txtDuration.Text),
+        FlightDate = dpDate.SelectedDate ?? DateTime.Now,
+        Takeoffs = 1,
+        Landings = 1
+    };
+
+    var db = new DatabaseHelper();
+    db.AddFlightLog(newLog);
+
+    MessageBox.Show("Flight recorded successfully!");
+    this.Close();
+}
